08d7c5d25e71a7cf39b113580ce39325ce77767b
ouga2450 <ouga.engneering@gmail.com>
2026-01-11 15:46:01 +0900

    test: HabitLogモデルのspec追加とFactoryBotのassociation修正

--- spec/factories/habit_log.rb
+++ spec/factories/habit_log.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :habit_log do
-    association :user
-    association :habit
+    association :goal
+    user { goal.user }
+    habit { goal.habit }
     started_at { Time.current }
     performed_value { 1 }
-    goal { build(:goal, user: user, habit: habit) }
 
     trait :with_ended_at do
       ended_at { started_at + 1.hour }
